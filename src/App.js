1315116bcc49bd092c85644aa866f469e117dfd1
Gabriele <g.callegari@itsrizzoli.it>
2023-02-21 17:28:04 +0100

    problemi con isLogged

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import './global'
+import Login from './Main/Login/Login'
 import Header from './Header/Header';
+
+//{global.config.isLogged === false ? <Login/> : <p>ciao</p> }
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './Header.css'
+import '../global'
 
 export default function Header(){
 
