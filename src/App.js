99c35c123f7f29e8e6b1480df9038b2229c82642
Gabriele <g.callegari@itsrizzoli.it>
2023-02-27 17:14:29 +0100

    add: profile component and main options

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { useEffect, useState } from "react";
 import Login from './Main/Login/Login'
 import Home from './Main/Home/Home';
+import Profile from "./Main/Profile/Profile"
 import { useCookies } from 'react-cookie';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
     if( cookies.name !== undefined){
       setStatus("Ciao "+cookies.name)
     }
+    // eslint-disable-next-line
   },[])
     
   function changeValue(){
@@ -25,16 +27,25 @@ function App() {
     setStatus("Ciao "+name)
   }
   
+  function GetBody(){
+    if(value === true && cookies.name === undefined){
+      return <Login/>
+    }else{
+      if(value === true && cookies.name !== undefined){
+        return <Profile/>
+      }else{
+        return <Home />
+      }
+    }
+  }
+
   return (
     <div className="App">
       <div className="header">
             <label className="header--label">Callezon</label>
             <label  className="header--status" onClick={changeValue}>{status}</label> 
         </div>
-        {value===true ? <Login callback={loginFromChild}/> : 
-        <div>
-         <Home/>   
-        </div>}
+        <GetBody />
     </div>
   );
 }
