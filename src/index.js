0f10a2614d8641b7f7245c9af10658f712fead3e
Gabriele <g.callegari@itsrizzoli.it>
2023-03-01 14:04:34 +0100

    add: screen orientation block

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 
 window.isLogged = false
+window.screen.orientation.lock("portrait")
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
