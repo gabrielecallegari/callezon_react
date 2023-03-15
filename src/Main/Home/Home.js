5eed8fc8542f46305fb89b2122f410fbcf1464f1
Gabriele <g.callegari@itsrizzoli.it>
2023-03-15 14:20:38 +0100

    add: added shopping cart

--- src/Main/Home/Home.js
+++ src/Main/Home/Home.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from "react";
 import './Home.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home(){
     // eslint-disable-next-line
     const [ products , setProducts] = useState(window.prod)
+    // eslint-disable-next-line
+    const [ carrello , setCarrello ] = useState(0)
 
+    function carrelloClick(){
+        setCarrello(old=> old+1)
+    }
+    
     useEffect(()=>{
         /*
         if(products === undefined){
@@ -23,6 +31,12 @@ export default function Home(){
     return (
         <div className="home">
             sei nella home
+            <div className="carrello" onClick={carrelloClick}>
+                <div className="carrello--div">
+                    <FontAwesomeIcon icon={faCartShopping} className="shoppingcart"/>
+                    {carrello>0 &&  <label className="carrello--size">{carrello}</label>}
+                </div>
+            </div>
         </div>
     )
 }
