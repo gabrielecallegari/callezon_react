e8e02290c04323cd698bcaa07d9f4cf0113ddb1f
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-03-16 17:48:14 +0100

    add: featured products

--- src/Main/Home/Detail/Detail.js
+++ src/Main/Home/Detail/Detail.js
@@ -1,9 +1,13 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import './Detail.css'
 
-export default function Detail(props){
+export default function Detail(){
     const { id } = useParams()
+    const router = useNavigate()
+    useEffect(()=>{
+        if(id>30) router("*")
+    })
     return (
         <div className="Details">
             {id}
--- src/Main/Home/Home.js
+++ src/Main/Home/Home.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import './Home.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faStar } from "@fortawesome/free-solid-svg-icons";
+import prodotti from "../../Database/Prodotti";
+import { useNavigate } from "react-router-dom";
 
 export default function Home(){
-    // eslint-disable-next-line
-    const [ products , setProducts] = useState(window.prod)
+    const router = useNavigate()
+    
+    
     
     
     useEffect(()=>{
@@ -19,7 +22,8 @@ export default function Home(){
             }) 
         }
         */
-    })
+       // eslint-disable-next-line
+    },[])
 
     const [ carrello , setCarrello ] = useState(0)
 
@@ -28,15 +32,45 @@ export default function Home(){
     }
 
     
+    var primoPiano = prodotti.products.filter(obj => {
+        
+        return obj.id === 1 || obj.id===2 || obj.id===4
+    })
+
+    const prodottiPP = primoPiano.map( element => {
+        const percent = parseInt(element.discountPercentage,10)
+        const rating = parseInt(element.rating ,10)
+        return (
+            <div key = {element.id} className="home--pp-prod" onClick={()=>router("/detail/"+element.id)}>
+                    <div className="home--pp-princ">
+                        <label className="home--pp-title">{element.title}</label>
+                        <img src={element.thumbnail} alt="Thumbnail" className="home--pp-img"></img>
+                        <div className="home--pp-info">
+                            <label className="home--pp-label2">Sconto: <b>{percent}%</b></label>
+                            <label className="home--pp-label2">Prezzo: {element.price}€</label>
+                            <div className="home--pp-raiting">
+                                <label className="home--pp-label2">Rating: {rating}</label>
+                                <FontAwesomeIcon icon={faStar} className="home--pp-icon"/>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        )
+    })
 
     return (
         <div className="home">
-            sei nella home
-
           <div className="carrello" onClick={carrelloClick}>
             <div className="carrello--div">
                 <FontAwesomeIcon icon={faCartShopping} className="shoppingcart"/>
                 {carrello>0 &&  <label className="carrello--size">{carrello}</label>}
+            </div>
+          </div>
+
+          <div className="home--primo-piano">
+            <label className="home--pp-label">Prodotti in primo piano</label>
+            <div className="home--pp">
+                {prodottiPP}
             </div>
           </div>
         
